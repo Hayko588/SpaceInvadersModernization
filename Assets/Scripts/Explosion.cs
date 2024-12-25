44b7d981ffed04831dd8047068504dcaab699b48
HaykPoghosyan <hayk.poghosyan@softconstruct.com>
2024-12-25 15:55:51 +0400

    [ADD] Explosion pool

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace DefaultNamespace {
+	public class Explosion : MonoBehaviour {
+		public class Pool : MemoryPool<Explosion> {
+			protected override void OnSpawned(Explosion item) {
+				item.Explode();
+			}
+			protected override void OnDespawned(Explosion item) {
+				item.Stop();
+			}
+		}
+
+		[SerializeField] private ParticleSystem _particleSystem;
+
+		private void Explode() {
+			_particleSystem.Play();
+		}
+
+		private void Stop() {
+			_particleSystem.Stop();
+			_particleSystem.Clear();
+		}
+	}
+}
